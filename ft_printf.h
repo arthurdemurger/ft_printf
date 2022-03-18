46dc8edebf81d4c75148f8b530ae7c4219e3c3c2
lirockk <98538729+lirockk@users.noreply.github.com>
2022-03-18 22:25:50 +0100

    Creation of the main files

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/18 22:21:16 by ademurge          #+#    #+#             */
+/*   Updated: 2022/03/18 22:22:53 by ademurge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+
+# define FT_PRINTF_H
+
+// Libraries
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+#endif
