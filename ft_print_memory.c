34201a62c0e5566a7d6d10311ce715420d4bb392
lirockk <arthurdemurger26@gmail.com>
2022-04-21 15:12:37 +0200

    Norminette

--- ft_print_memory.c
+++ ft_print_memory.c
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 16:08:50 by ademurge          #+#    #+#             */
-/*   Updated: 2022/04/21 15:09:01 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/04/21 15:12:13 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,14 @@ static void	ft_putchar_mem(int nbr, char *base, int *count)
 
 static void	ft_putmem(unsigned long nbr, int *count)
 {
-	if (nbr >  15)
+	if (nbr > 15)
 		ft_putmem(nbr / 16, count);
 	ft_putchar_mem((nbr % 16), "0123456789abcdef", count);
 }
+
 void	ft_print_address(void *add, int *count)
 {
-	unsigned long hex;
+	unsigned long	hex;
 
 	hex = (unsigned long)add;
 	ft_putstr("0x", count);
